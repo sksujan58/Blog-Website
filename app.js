d710d08522cc765f6b9bcf629b7f3b0a42b0072d
Sujan <786sksujanislam786@gmail.com>
2021-01-16 23:29:46 +0530

    updated commit

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ res.redirect("/")
 })
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(5000, function() {
+  console.log("Server started on port 5000");
 });
