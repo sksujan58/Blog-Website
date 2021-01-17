d94171a79ad1a294d0cb4529adaeb03a9ec24ebc
Sujan <786sksujanislam786@gmail.com>
2021-01-17 20:23:32 +0530

    database added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose=require("mongoose")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,10 +19,24 @@ app.use(express.static("public"));
 
 const arrPost=[]
 
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema=mongoose.Schema({
+  title:String,
+  content:String
+})
+
+const Post=mongoose.model("Post",postSchema);
+
 
 
 app.get("/", function (req,res){
-  res.render("home", {homeContent:homeStartingContent , new_posts:arrPost})
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      homeContent: homeStartingContent,
+      new_posts: posts
+      });
+  });
 })
 
 app.get("/about", function (req,res){
@@ -39,19 +54,20 @@ app.get("/compose", function (req,res){
 
 
 
-app.get('/posts/:postName', function (req, res) {
-  var posttitle= _.lowerCase(req.params.postName);
+app.get('/posts/:postId', function (req, res) {
+  var reqId= req.params.postId;
   
- arrPost.forEach(function (object){
+  Post.findOne({_id: reqId}, function(err, post){
 
-  var storedTitle=_.lowerCase(object.Title);
-
-   if(storedTitle==posttitle){
-    
-    res.render("post",{title:object.Title, postContent:object.Post})
-
-   }
- })
+    res.render("post", {
+ 
+      title: post.title,
+ 
+      postContent: post.content
+ 
+    });
+ 
+  });
 })
 
 
@@ -62,14 +78,15 @@ app.get('/posts/:postName', function (req, res) {
 
 app.post("/compose",function(req,res){
 
-  var title=req.body.title;
+  var postTitle=req.body.title;
   var details=req.body.details;
-  const obj={
-    "Title":title,
-    "Post":details
-  }
 
-arrPost.push(obj)
+  const newPost= new Post({
+    title:postTitle,
+    content:details
+  })
+
+newPost.save()
 
 res.redirect("/")
 })
